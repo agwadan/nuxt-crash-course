27160748a6f4fa7e2b23c350efd528ac030c3b45
agwadan <agwa.dan@gmail.com>
2022-01-16 22:57:41 +0300

    Update index page and add navs

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>The baddest dad jokes on the web.</h1>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  head() {
+    return {
+      title: "Welcome To Dad Jokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for bad dad jokes",
+        },
+      ],
+    };
+  },
+};
 </script>
